e16132fc14ce029b8694cc81639ab8d166f50ff8
Stephen Jenkinson <stephen.jenkinson@tattsgroup.com>
2017-03-03 13:10:09 +1000

    Default Tipping as primary route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
 const appRoutes: Routes = [
-  { path: '', loadChildren: 'app/sport/sport.module#SportModule' },
-  { path: 'sport', loadChildren: 'app/sport/sport.module#SportModule' },
-  { path: 'tipping', loadChildren: 'app/tipping/tipping.module#TippingModule' }
+  { path: '', loadChildren: 'app/tipping/tipping.module#TippingModule' },
+  { path: 'tipping', loadChildren: 'app/tipping/tipping.module#TippingModule' },
+  { path: 'sport', loadChildren: 'app/sport/sport.module#SportModule' }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
